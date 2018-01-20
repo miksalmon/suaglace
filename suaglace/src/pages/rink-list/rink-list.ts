b27ebede6a1d9d4c20b0a5d468458b3a44818b06
Unknown <samuel.chapleau@polymtl.ca>
2018-01-20 13:42:21 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ suaglace/src/pages/rink-list/rink-list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-rink-list',
+  templateUrl: 'rink-list.html'
+})
+export class RinkListPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
