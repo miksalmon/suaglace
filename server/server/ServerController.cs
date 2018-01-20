df89e13c1c3f6c4426e12646f52da94205335e80
Michael Salmon <mikk_salmon@hotmail.com>
2018-01-20 15:06:10 -0500

    add server controller

---
+++ server/server/ServerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinSelfhostSample
+{
+    public class ServerController : ApiController
+    {
+        // GET api/server 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/server/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/server 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/server/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/server/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
