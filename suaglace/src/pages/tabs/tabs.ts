598c1e57047a51e7dd4454f468fc7cca8a0b3149
Unknown <samuel.chapleau@polymtl.ca>
2018-01-20 13:43:13 -0500

    Auto stash before merge of "master" and "origin/master"

--- suaglace/src/pages/tabs/tabs.ts
+++ suaglace/src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { RinkListPage } from '../rink-list/rink-list';
+import { ProfilPage } from '../profil/profil';
+import { MapPage } from '../map/map';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MapPage;
+  tab2Root = RinkListPage;
+  tab3Root = ProfilPage;
 
   constructor() {
 
