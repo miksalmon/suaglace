4bfadd2802b1f80123334a305e1c54d45bc3f0d7
Unknown <samuel.chapleau@polymtl.ca>
2018-01-20 22:06:55 -0500

    Add http get all rinks

--- suaglace/src/app/app.module.ts
+++ suaglace/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HTTP } from '@ionic-native/http';
 
 import { RinkListPage } from '../pages/rink-list/rink-list';
 import { ProfilPage } from '../pages/profil/profil';
@@ -37,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HTTP
   ]
 })
 export class AppModule {}
