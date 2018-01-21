5fa8d0879d70a555932ba3408f1cac6d012778ea
Vincent Labonté <vincent.labonte31@gmail.com>
2018-01-21 07:05:41 -0500

    Add a Get handler that send the rink of a given player

---
+++ server/server/PlayerRinksController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace server
+{
+    public class PlayerrinksController : ApiController
+    {
+
+        // GET api/playerrinks/paul
+        public string Get([FromUri] string id)
+        {
+            foreach (KeyValuePair<string, List<User>> item in RinkManager.CurrentPlayers)
+            {
+                List<User> players = item.Value;
+                if (players.Exists(player => player.Name == id))
+                {
+                    return item.Key;
+                }
+            }
+
+            return null;
+        }
+
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
