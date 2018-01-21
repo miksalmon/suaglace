c2eab1d0edfdad2967a33136df1913e2b057216a
Francis F <f.forget@polymtl.ca>
2018-01-21 03:45:25 -0500

    Added buttons to the bottom of the screen

--- suaglace/src/pages/map/map.ts
+++ suaglace/src/pages/map/map.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 declare var google: any;
 
@@ -12,8 +11,6 @@ export class MapPage {
   @ViewChild('map') mapRef: ElementRef;
 
   map: any;
-
-  constructor(public navCtrl: NavController) { }
 
   ionViewDidLoad() {
     this.showMap();
@@ -43,9 +40,6 @@ export class MapPage {
       '<h5 id="firstHeading" class="firstHeading">Patinoire HEC</h5>' +
       '<div id="bodyContent">' +
       '<b>Adresse</b> : 3000 Chemin de la Côte-Sainte-Catherine, Montréal, QC H3T 2A7' +
-      '<br>' + '<br>' +
-      '<button type="button" class="btn btn-outline-primary btn-lg viewDetails">Reserve</button>' +
-      '<button type="button" class="btn btn-primary btn-lg viewDetails">View Details</button>' +
       '</div>' +
       '</div>';
 
@@ -59,8 +53,21 @@ export class MapPage {
       map: this.map,
       title: 'HEC Montreal'
     });
-    marker.addListener('click', function () {
+    marker.addListener('click', () => {
       infowindow.open(this.map, marker);
     });
+
+    google.maps.event.addDomListener(marker, 'click', function() {
+      console.log("test")
+  });
+
   }
+  openDetails() {
+    console.log("Open Details");
+  }
+
+  openReserve() {
+    console.log("Open Reserve");
+  }
+
 }
