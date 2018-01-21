5e6b0ce93023cc2206098207a59e904a791090b8
Francis F <f.forget@polymtl.ca>
2018-01-20 22:07:41 -0500

    Refined Google Maps Info Window

--- suaglace/src/pages/map/map.ts
+++ suaglace/src/pages/map/map.ts
@@ -25,11 +25,43 @@ export class MapPage {
 
     const options = {
       center: location,
-      zoom: 10
+      zoom: 15,
+      streetViewControl: false,
+      scaleControl: false,
+      mapTypeControl: false,
+      zoomControl: false,
+      clickableIcons: false,
+      fullscreenControl: false
     }
 
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    const HEC = new google.maps.LatLng(45.503363, -73.620758);
 
+
+    const contentString = '<div id="content">' +
+      '<div id="siteNotice">' +
+      '</div>' +
+      '<h5 id="firstHeading" class="firstHeading">Patinoire HEC</h5>' +
+      '<div id="bodyContent">' +
+      '<b>Adresse</b> : 3000 Chemin de la Côte-Sainte-Catherine, Montréal, QC H3T 2A7' +
+      '<br>' + '<br>' +
+      '<button type="button" class="btn btn-outline-primary btn-lg viewDetails">Reserve</button>' +
+      '<button type="button" class="btn btn-primary btn-lg viewDetails">View Details</button>' +
+      '</div>' +
+      '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200
+    });
+
+    var marker = new google.maps.Marker({
+      position: HEC,
+      map: this.map,
+      title: 'HEC Montreal'
+    });
+    marker.addListener('click', function () {
+      infowindow.open(this.map, marker);
+    });
   }
-
 }
